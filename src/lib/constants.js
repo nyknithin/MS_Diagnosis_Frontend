f1a8a48bc598e89e0b52be5034022f970484399f
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2025-09-21 12:02:22 +0530

    chore(constants): add appName and navLinks to constants

---
+++ src/lib/constants.js
@@ -0,0 +1,7 @@
+export const appName = "MS Diagnosis";
+
+export const navLinks = [
+  { path: "/", label: "Home" },
+  { path: "/diagnosis", label: "Diagnosis" },
+  { path: "/contact", label: "Contact" }
+];
