1add9055849fab22cd3571f26824e50f30953935
Karthik Sapaliga <im.karthiksapaliga@gmail.com>
2025-09-21 11:04:39 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+    <h1>MS Diagnosis</h1>
+</div>
+
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center font-bold text-2xl'>Testing Setup</div>
+  )
+}
+
+export default App
